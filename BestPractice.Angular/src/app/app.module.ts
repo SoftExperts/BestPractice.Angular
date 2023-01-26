18fb3729c8944adabd7ffde0affd88cda70ef1bb
Asif Saeed <asifsaeed.softexpert@gmail.com>
2023-01-27 00:18:08 +0500

    Added Content Component
    Added Content Component

--- BestPractice.Angular/src/app/app.module.ts
+++ BestPractice.Angular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContentComponentComponent } from './content-component/content-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentComponentComponent
   ],
   imports: [
     BrowserModule,
